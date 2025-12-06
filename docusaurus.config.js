d6c5558f3ff7aba55a8b47f0138112f2e8b8071b
dev-sire <shahidamaan2019@gmail.com>
2025-12-06 16:10:15 +0500

    added final documentations

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,17 +17,10 @@ const config = {
   title: 'Physical AI & Humanoid Robotics Book',
   tagline: 'A guide to bridging digital AI with real-world robotic systems.',
   favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'your-org', // Usually your GitHub org/user name.
-  projectName: 'physical-ai-book', // Usually your repo name.
+  url: 'https://dev-sire.github.io',
+  baseUrl: '/Humanoid-AI-Book/',
+  organizationName: 'dev-sire',
+  projectName: 'Humanoid-AI-Book',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
